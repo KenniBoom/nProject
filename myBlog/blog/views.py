c31a4462cf33a497cbf6505d5da30a0ce04657a9
KenniBoom <karakurtas1@gmail.com>
2022-07-17 14:33:40 +0300

    first commit

---
+++ myBlog/blog/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+ 
+ 
+def home(request):
+    return render(request, "partial/home.html")
+
+def single(request, id=None):
+    return render(request, "partial/single.html")
+
+# Create your views here.
